82f11ccd761fde4108174323a2480e4b7594f4cf
AYAN KOLAY <91792348+Ayan-Kolay@users.noreply.github.com>
2023-12-11 23:33:48 +0530

    fixed broken changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -31,7 +31,7 @@ function Signup() {
     
 
 <section>
-    <div className="grid grid-cols-1 lg:grid-cols-2">
+    <div className="grid grid-cols-1">
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
             <span className="inline-block w-full max-w-[100px]">
@@ -93,11 +93,7 @@ function Signup() {
           </div>
         </div>
         <div className="h-full w-full">
-          <img
-            className="mx-auto h-full w-full rounded-md object-cover"
-            src="https://images.unsplash.com/photo-1630673245362-f69d2b93880e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-            alt=""
-          />
+          
         </div>
       </div>
  </section>
