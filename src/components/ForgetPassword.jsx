3338a1e6c5d237f953568c2ec60af447c3302a78
AYAN KOLAY <91792348+Ayan-Kolay@users.noreply.github.com>
2023-12-12 00:00:47 +0530

    Reset Password Added

--- src/components/ForgetPassword.jsx
+++ src/components/ForgetPassword.jsx
@@ -18,28 +18,75 @@ function ForgetPassword() {
         }
     }
   return (
-    <div>
-      <div className='flex items-center justify-center w-full'>
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
-        <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
+    // <div>
+    //   <div className='flex items-center justify-center w-full'>
+    //     <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+    //     <div className="mb-2 flex justify-center">
+    //                 <span className="inline-block w-full max-w-[100px]">
+    //                     <Logo width="100%" />
+    //                 </span>
+    //     </div>
+    //     <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+    //     <p className="mt-2 text-center text-base text-black/60">
+    //                 Don&apos;t have any account?&nbsp;
+    //                 <Link
+    //                     to="/signup"
+    //                     className="font-medium text-primary transition-all duration-200 hover:underline"
+    //                 >
+    //                     Sign Up
+    //                 </Link>
+    //     </p>
+    //     {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+    //     <form onSubmit={handleSubmit(submit)} className='mt-8'>
+    //         <div className='space-y-5'>
+    //             <Input
+    //             label="Email: "
+    //             placeholder="Enter your email"
+    //             type="email"
+    //             {...register("email", {
+    //                 required: true,
+    //                 validate: {
+    //                     matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+    //                     "Email address must be a valid address",
+    //                 }
+    //             })}
+    //             /><Button
+    //             type="submit"
+    //             className="w-full"
+    //             >Submit</Button>
+    //         </div>
+    //     </form>
+    //     </div>
+    //     </div>
+    //     </div>
+
+    <section>
+      <div className="grid grid-cols-1">
+        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+    <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
-        </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-        <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
-                    <Link
-                        to="/signup"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
-                    >
-                        Sign Up
-                    </Link>
-        </p>
-        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(submit)} className='mt-8'>
-            <div className='space-y-5'>
-                <Input
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in</h2>
+            <p className="mt-2 text-sm text-gray-600">
+              Don&apos;t have an account?{' '}
+              <Link
+    to="/signup"
+    className="font-medium text-primary transition-all duration-200 hover:underline"
+>
+    Sign Up
+</Link>
+            </p>
+    {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+            <form  method="POST" className="mt-8" onSubmit={handleSubmit(submit)} >
+              <div className="space-y-5">
+                <div>
+                  <label htmlFor="" className="text-base font-medium text-gray-900">
+                    {' '}
+                    Email address{' '}
+                  </label>
+                  <div className="mt-2">
+                  <Input
                 label="Email: "
                 placeholder="Enter your email"
                 type="email"
@@ -55,10 +102,19 @@ function ForgetPassword() {
                 className="w-full"
                 >Submit</Button>
             </div>
-        </form>
+                </div>
+              </div>
+            </form>
+            <div className="mt-3 space-y-3">
+
+            </div>
+          </div>
         </div>
+        <div className="h-full w-full">
+         
         </div>
-        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -27,20 +27,23 @@ function ResetPassword() {
         }
     }
   return (
-    <div>
-      <div className='flex items-center justify-center w-full'>
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
-        <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
+
+<section>
+      <div className="grid grid-cols-1">
+        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+    <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
-        </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Update the password</h2>
-        
-        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(submit)} className='mt-8'>
-            <div className='space-y-5'>
-                <Input
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Update the Password</h2>
+           
+    {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+            <form  method="POST" className="mt-8" onSubmit={handleSubmit(submit)} >
+              <div className="space-y-5">
+                <div>
+                  
+                  <div className="mt-2">
+                  <Input
                 label="Password: "
                 placeholder="Enter your password"
                 type="text"
@@ -63,10 +66,61 @@ function ResetPassword() {
                 className="w-full"
                 >Update</Button>
             </div>
-        </form>
+                </div>
+              </div>
+            </form>
+            <div className="mt-3 space-y-3">
+
+            </div>
+          </div>
         </div>
+        <div className="h-full w-full">
+         
         </div>
-        </div>
+      </div>
+    </section>
+
+
+    // <div>
+    //   <div className='flex items-center justify-center w-full'>
+    //     <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+    //     <div className="mb-2 flex justify-center">
+    //                 <span className="inline-block w-full max-w-[100px]">
+    //                     <Logo width="100%" />
+    //                 </span>
+    //     </div>
+    //     <h2 className="text-center text-2xl font-bold leading-tight">Update the password</h2>
+        
+    //     {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+    //     <form onSubmit={handleSubmit(submit)} className='mt-8'>
+    //         <div className='space-y-5'>
+    //             <Input
+    //             label="Password: "
+    //             placeholder="Enter your password"
+    //             type="text"
+    //             {...register("password", {
+    //                 required: true,
+                    
+    //             })}
+    //             />
+    //             <Input
+    //             label="Confirm Password: "
+    //             placeholder="Enter your password Again"
+    //             type="password"
+    //             {...register("passwordAgain", {
+    //                 required: true,
+                    
+    //             })}
+    //             />
+    //             <Button
+    //             type="submit"
+    //             className="w-full"
+    //             >Update</Button>
+    //         </div>
+    //     </form>
+    //     </div>
+    //     </div>
+    //     </div>
   )
 }
 
